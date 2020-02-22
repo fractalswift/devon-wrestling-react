25e46b563d8314185619fcbe9c434bbdf67122e4
fractalswift <matthewthomasmacklin@gmail.com>
2020-02-22 13:19:36 +0000

    Updates

---
+++ precache-manifest.92832d59ce6a7143855c017b98d1954f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c15e1959affcc3db3644598ffbdb4c67",
+    "url": "/devon-wrestling-react/index.html"
+  },
+  {
+    "revision": "b47014c8175405a124a7",
+    "url": "/devon-wrestling-react/static/css/main.f1edb4df.chunk.css"
+  },
+  {
+    "revision": "02ccd2a65caa645d5c5f",
+    "url": "/devon-wrestling-react/static/js/2.d3994bf4.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "/devon-wrestling-react/static/js/2.d3994bf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b47014c8175405a124a7",
+    "url": "/devon-wrestling-react/static/js/main.3435125a.chunk.js"
+  },
+  {
+    "revision": "524d57b9cc6a60168c92",
+    "url": "/devon-wrestling-react/static/js/runtime-main.25430d61.js"
+  }
+]);
